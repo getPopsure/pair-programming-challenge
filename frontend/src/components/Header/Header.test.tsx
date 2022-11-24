25a86762966a37248850ec1cb079fce3896cc151
Diogo Mateus <diogoamateus@gmail.com>
2022-11-24 17:06:04 +0000

    Update Header component to allow content

--- frontend/src/components/Header/Header.test.tsx
+++ frontend/src/components/Header/Header.test.tsx
@@ -5,8 +5,14 @@ describe('Components/Header', () => {
   describe('Normal state', () => {
     it('Should correctly display a title', () => {
       const title = 'Policies';
-      render(<Header>{title}</Header>);
+      render(<Header title={title} />);
       screen.getByRole('heading', { name: title });
+    });
+
+    it('Should correctly display Content', () => {
+      const title = 'Policies';
+      render(<Header title={title}>Content</Header>);
+      screen.getByText('Content');
     });
   });
 });
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import { ReactNode } from 'react';
+
 interface HeaderProps {
-  children: string;
+  children?: ReactNode;
+  title: string;
 }
 
-export const Header = ({ children }: HeaderProps) => {
-  return <h1 className="text-3xl font-bold text-gray-900 mb-2">{children}</h1>;
+export const Header = ({ children, title }: HeaderProps) => {
+  return (
+    <header className="md:flex justify-between">
+      <h1 className="text-3xl font-bold text-gray-900 mb-2">{title}</h1>
+      <div className="w-64">{children}</div>
+    </header>
+  );
 };
